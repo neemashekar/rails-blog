927b036997472e03696c6d46ab1612584ca5554f
chandra shekar <neema.shekar@snc.edu>
2017-11-21 16:00:40 -0600

    fixed a config prob

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'Men')
+Category.create(name: 'Family')
+Category.create(name: 'Friends')
+
+Post.create(title: 'Guys suck!', content: "They really do though...", category_id: 1)
+Post.create(title: 'Family Love', content: "Leaving for the Middle East soon. Can't wait to spend time with the fam!", category_id: 2)
+Post.create(title: 'Best Friends in the City', content: "The only good thing about being in DBC is meeting my best friend Aminah!!", category_id: 3)
